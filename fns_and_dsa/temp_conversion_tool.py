c2171e0d3c6c6f085817763c39507b71a92402ce
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-10-29 23:04:02 +0100

    Add a functionality to check ValueError

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,15 +8,19 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
+try:
+    temperature = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 
-temperature = float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+    if unit == "F":
+        converted = convert_to_celsius(temperature)
+        print(f"{temperature}°C is {converted}F°")
+    elif unit == "C":
+        converted = convert_to_fahrenheit(temperature)
+        print(f"{temperature}F° is {converted}°C")
+    else:
+        print("Enter a valid unit C or F.")
 
-if unit == "F":
-    converted = convert_to_celsius(temperature)
-    print(f"{temperature}°C is {converted}F°")
-elif unit == "C":
-    converted = convert_to_fahrenheit(temperature)
-    print(f"{temperature}F° is {converted}°C")
-else:
-    print("Enter a valid unit C or F.")
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
+    
