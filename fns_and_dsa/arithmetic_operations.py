860ce1a9d87569982e51d84f56f81e380c1f95e7
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-10-29 13:31:17 +0100

    0. Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation(num1, num2, operation) :
+    match operation :
+        case "add" :
+            result = num1 + num2
+            return result
+        case "subtract" :
+            result = num1 - num2
+            return result
+        case "muliply" :
+            result = num1 * num2
+            return result
+        case "divide" :
+            if num2 == 0 :
+                return "Error: Cannot divide by zero!"
+            else :
+                result = num1 / num2
+                return result
+        case _ :
+            return "Error: Invalid operation"
+        
+
+
